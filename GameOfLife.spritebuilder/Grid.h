fa5cb66728a1c5b2eea099625df3bd0adddd74ef
mengsquared <mengmengjie@0587373822.wireless.umich.net>
2014-08-28 15:15:09 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -12,5 +12,8 @@
 
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
-
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
+- (BOOL)isIndexValidForX:(int)x andY:(int)y;
 @end
